5acecf7e8602ac1cb5f07e3cf1f348583bdce711
J Rose <josh@upros3.com>
2018-01-23 20:00:09 +0000

    first commit, server built, in process of building templates

---
+++ express_server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+
+app.set("view engine", "ejs");
+
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n")
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
